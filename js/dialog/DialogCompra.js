b5bad4bf48447a35c5fcc02def63ca325b377f19
jaidson_mda <>
2018-08-18 06:46:01 +0000

    Commmit pelo team viewr

--- js/dialog/DialogCompra.js
+++ js/dialog/DialogCompra.js
@@ -1,34 +1,38 @@
-class DialogCompra{
-	constructor(){
-    let $ = document.querySelector.bind(document);
-		this._dialogCadastra= $('#cadastraItem');
-    	this._dialog = $('#dialogCompraView');
-      this._dialogExclui = $('#excluiCompra');
-      this._dialogExcluiTudo = $('#excluiCompraTudo');
-    	if (! this._dialog.showModal) {
-      		dialogPolyfill.registerDialog(this._dialog);
-    	}
-    	if (! this._dialogCadastra.showModal) {
-      		dialogPolyfill.registerDialog(this._dialogCadastra);
-    	}
-      if (! this._dialogExcluiTudo.showModal) {
-          dialogPolyfill.registerDialog(this._dialogExcluiTudo);
-      }
-      if (! this._dialogExclui.showModal) {
-          dialogPolyfill.registerDialog(this._dialogExclui);
-      }
-	}
-    showDialogButton(){
-      this._dialog.showModal();
+class DialogCompra {
+    constructor() {
+        let $ = document.querySelector.bind(document);
+        this._dialogCadastra = $('#cadastraItem');
+        this._dialog = $('#dialogCompraView');
+        this._dialogExclui = $('#excluiCompra');
+        this._dialogExcluiTudo = $('#excluiCompraTudo');
+        if (!this._dialog.showModal) {
+            dialogPolyfill.registerDialog(this._dialog);
+        }
+        if (!this._dialogCadastra.showModal) {
+            dialogPolyfill.registerDialog(this._dialogCadastra);
+        }
+        if (!this._dialogExcluiTudo.showModal) {
+            dialogPolyfill.registerDialog(this._dialogExcluiTudo);
+        }
+        if (!this._dialogExclui.showModal) {
+            dialogPolyfill.registerDialog(this._dialogExclui);
+        }
     }
-    showDialogCadastra(){
-      this._dialogCadastra.showModal();
+
+    showDialogButton() {
+        this._dialog.showModal();
     }
-    showExclui(){
-      this._dialogExclui.showModal();
+
+    showDialogCadastra() {
+        this._dialogCadastra.showModal();
     }
-    showExcluiTudo(){
-      this._dialogExcluiTudo.showModal();
+
+    showExclui() {
+        this._dialogExclui.showModal();
+    }
+
+    showExcluiTudo() {
+        this._dialogExcluiTudo.showModal();
     }
     close(){
       if(this._dialog.open){
